3863f34c5689554684aaa5caa56a310c0c988230
Fae <FaeSerrano@Faes-MacBook-Air.local>
2016-11-17 22:06:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/faeserrano/Activity5/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.faeserrano.Activity5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void showGreetings(View view) {
+        String button_text;
+        button_text = ((Button) view).getText().toString();
+
+        if (button_text.equals("Open Second Activity")) {
+
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        }
+        else if (button_text.equals("Open Third Activity")) {
+            Intent intent = new Intent(this, ThirdActivity.class);
+            startActivity(intent);
+        }
+    }
+
+}
